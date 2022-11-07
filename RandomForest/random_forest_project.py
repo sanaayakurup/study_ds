bf569c6f4a899b2a3cc631d2cb0112d827d9501c
sanaayakurup <sanaayakurup@gmail.com>
2022-11-07 16:04:37 +0530

    random_forest

---
+++ RandomForest/random_forest_project.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 16:00:47 2022
+
+@author: Sanaaya
+"""
+
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestRegressor
--- cross_validation_and_model_selectio/Model_selection.py
+++ cross_validation_and_model_selectio/Model_selection.py
@@ -16,6 +16,7 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
 heart_data=pd.read_csv('heart.csv')
 heart_data.head()
@@ -45,23 +46,62 @@ compare_score()
 
 
 ##Comparing models with different hyperparam values
-models=[LogisticRegression(max_iter=10000),SVC(),DecisionTreeClassifier(),KNeighborsClassifier(),RandomForestClassifier()]
+models = [LogisticRegression(max_iter=10000), SVC(), KNeighborsClassifier(), RandomForestClassifier(random_state=0)]
 model_hyperparams={
-    'log_hps':{
-        'C':[1,5,10,20]},
-    'SVC_hps':{
-        'kernel':['linear','poly','sigmoid','rbf']
-        ,'C':[1,5,10,20]
-        },
-    'KNN_hps':{
-        'n_neighbours':[3,5,10]
-        },
-    'RandomForest_hps':{
-        'n_estimators':[10,20,50,100]
-        }        
-        }
+    
+
+    'log_reg_hyperparameters': {
+        
+        'C' : [1,5,10,20]
+    },
+
+    'svc_hyperparameters': {
+        
+        'kernel' : ['linear','poly','rbf','sigmoid'],
+        'C' : [1,5,10,20]
+    },
+
+
+    'KNN_hyperparameters' : {
+        
+        'n_neighbors' : [3,5,10]
+    },
+
+
+    'random_forest_hyperparameters' : {
+        
+        'n_estimators' : [10, 20, 50, 100]
+    }
+}
     
 type(model_hyperparams)    
 model_hyperparams.keys()
 model_hyperparams.values()
 model_keys=list(model_hyperparams.keys())
+model_hyperparams[model_keys[0]]
+
+
+#def a model selection function with various hyperparams
+def model_selection_hps(models,model_hyperparams):
+    result=[]
+    i=0
+    for model in models:
+        key=model_keys[i]
+        params=model_hyperparams[key]
+        i=i+1
+        print(model)
+        print(params)
+        #now we have the model name and the model params 
+        classifier=GridSearchCV(model,params,cv=5)
+        classifier.fit(X,Y)
+        result.append(
+            {
+            'model_use':key,
+            'highest_score':classifier.best_score_,
+            'best_param':classifier.best_params_
+            }
+            )
+        result_df=pd.DataFrame(result,columns=['model_used','highest_score','best_param'])  
+    return result_df
+
+results=model_selection_hps(models,model_hyperparams)
